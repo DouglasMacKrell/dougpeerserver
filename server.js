aedac0793ea969d4f3b385df78b82040d74ebd44
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-05-24 11:42:54 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+var cors = require('cors')
+app.use(cors())
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+const http = require('http');
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
+
+server.listen(9000);
